261a203b6b3950dbd934a68ecaae6fd5317fd001
kimbob36 <kim.laatsch@yahoo.de>
2026-04-11 10:32:25 +0100

    Add files via upload

---
+++ git.tsx
@@ -0,0 +1,27 @@
+import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { ClientOnly } from 'remix-utils/client-only';
+import { BaseChat } from '../components/chat/BaseChat';
+import { GitUrlImport } from '../components/git/GitUrlImport.client';
+import { Header } from '../components/header/Header';
+import BackgroundRays from '../components/ui/BackgroundRays';
+
+export const meta: MetaFunction = () => {
+  return [{ title: 'Bolt' }, { name: 'description', content: 'Talk with Bolt AI' }];
+};
+
+export async function loader(args: LoaderFunctionArgs) {
+  return json({ url: args.params.url });
+}
+
+export default function Index() {
+  return (
+    <div className="flex flex-col h-full w-full bg-bolt-elements-background-depth-1">
+      <BackgroundRays />
+      <Header />
+      <ClientOnly fallback={<BaseChat />}>
+        {() => <GitUrlImport />}
+      </ClientOnly>
+    </div>
+  );
+}
